ed26b6151891acbc16e210704d2dc7a7806fe55b
saeveritt <saeveritt@gmail.com>
2016-11-10 22:22:20 -0600

    Update with bitcoin mainnet and testnet support

--- pypeerassets/networks.py
+++ pypeerassets/networks.py
@@ -3,7 +3,6 @@ from collections import namedtuple
 Network = namedtuple('Network', [
     'network_name',
     'network_shortname',
-    'subnet_name',
     'pubkeyhash',
     'wif_prefix',
     'scripthash',
@@ -12,9 +11,13 @@ Network = namedtuple('Network', [
 
 networks = (
     # Peercoin mainnet
-    Network("Peercoin", "ppc", "mainnet", b'37', b'b7', b'75', b'e6e8e9e5'),
+    Network("Peercoin", "ppc", b'37', b'b7', b'75', b'e6e8e9e5'),
     # Peercoin testnet
-    Network("Peercoin", "tppc", "testnet", b'6f', b'ef', b'c4', b'cbf2c0ef')
+    Network("Peercoin-testnet", "tppc", b'6f', b'ef', b'c4', b'cbf2c0ef'),
+    # Bitcoin mainnet
+    Network("Bitcoin", "btc", b'00', b'80', b'05', b'd9b4bef9'),
+    # Bitcoin testnet
+    Network("Bitcoin-testnet", "tbtc", b'6f', b'ef', b'c4', b'dab5bffa')
 )
 
 def query(query):
